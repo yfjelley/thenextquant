b6b915a192cb9b5b62d8749f1bfefda0598c83f0
huangtao <huangtao@ifclover.com>
2019-05-30 19:54:47 +0800

    update demo strategy

--- example/strategy/strategy.py
+++ example/strategy/strategy.py
@@ -9,6 +9,8 @@ from quant.config import config
 from quant.market import Market
 from quant.trade import Trade
 from quant.const import BINANCE
+from quant.order import Order
+from quant.market import Orderbook
 from quant.order import ORDER_ACTION_BUY, ORDER_STATUS_FAILED, ORDER_STATUS_CANCELED, ORDER_STATUS_FILLED
 
 
@@ -40,18 +42,21 @@ class MyStrategy:
         # 订阅行情
         Market(const.MARKET_TYPE_ORDERBOOK, const.BINANCE, self.symbol, self.on_event_orderbook_update)
 
-    async def on_event_orderbook_update(self, orderbook):
+    async def on_event_orderbook_update(self, orderbook: Orderbook):
         """ 订单薄更新
         """
         logger.debug("orderbook:", orderbook, caller=self)
-        bid3_price = orderbook["bids"][2][0]  # 买三价格
-        bid4_price = orderbook["bids"][3][0]  # 买四价格
+        bid3_price = orderbook.bids[2][0]  # 买三价格
+        bid4_price = orderbook.bids[3][0]  # 买四价格
 
         # 判断是否需要撤单
         if self.order_no:
             if float(self.create_order_price) < float(bid3_price) or float(self.create_order_price) > float(bid4_price):
                 return
-            await self.trader.revoke_order(self.order_no)
+            _, error = await self.trader.revoke_order(self.order_no)
+            if error:
+                logger.error("revoke order error! error:", error, caller=self)
+                return
             self.order_no = None
             logger.info("revoke order:", self.order_no, caller=self)
 
@@ -61,17 +66,15 @@ class MyStrategy:
         action = ORDER_ACTION_BUY
         price = tools.float_to_str(new_price)
         quantity = tools.float_to_str(quantity)
-        order_no = await self.trader.create_order(action, price, quantity)
+        order_no, error = await self.trader.create_order(action, price, quantity)
+        if error:
+            logger.error("create order error! error:", error, caller=self)
+            return
         self.order_no = order_no
         self.create_order_price = price
         logger.info("create new order:", order_no, caller=self)
 
-    async def on_event_asset_update(self, asset):
-        """ 资产更新
-        """
-        logger.info("asset:", asset, caller=self)
-
-    async def on_event_order_update(self, order):
+    async def on_event_order_update(self, order: Order):
         """ 订单状态更新
         """
         logger.info("order update:", order, caller=self)
