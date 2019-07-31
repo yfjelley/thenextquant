ca3a762e2c5efda60cbcabd61a0b18a6508f2c4e
huangtao <huangtao@ifclover.com>
2019-07-31 17:19:04 +0800

    Translate to English

--- quant/event.py
+++ quant/event.py
@@ -1,13 +1,11 @@
 # -*— coding:utf-8 -*-
 
 """
-事件处理中心
+Event Center.
 
 Author: HuangTao
 Date:   2018/05/04
-Update: 2018/09/26  1. 优化回调函数由exchange和routing_key确定；
-        2018/11/23  1. 事件增加批量订阅消息类型；
-        2018/11/28  1. 增加断线重连机制；
+Email:  huangtao@ifclover.com
 """
 
 import json
@@ -32,25 +30,26 @@ __all__ = ("EventCenter", "EventConfig", "EventHeartbeat", "EventAsset", "EventO
 
 
 class Event:
-    """ 事件
+    """ Event base.
+
+    Attributes:
+        name: Event name.
+        exchange: Exchange name.
+        queue: Queue name.
+        routing_key: Routing key name.
+        pre_fetch_count: How may message per fetched, default is 1.
+        data: Message content.
     """
 
     def __init__(self, name=None, exchange=None, queue=None, routing_key=None, pre_fetch_count=1, data=None):
-        """ 初始化
-        @param name 事件名
-        @param exchange 事件被投放的RabbitMQ交换机
-        @param queue 事件被路由的RabbitMQ队列
-        @param routing_key 路由规则
-        @param pre_fetch_count 每次从消息队列里获取处理的消息条数，越多处理效率越高，但同时消耗内存越大，对进程压力也越大
-        @param data 待发布事件的数据
-        """
+        """Initialize."""
         self._name = name
         self._exchange = exchange
         self._queue = queue
         self._routing_key = routing_key
         self._pre_fetch_count = pre_fetch_count
         self._data = data
-        self._callback = None  # 事件回调函数
+        self._callback = None  # Asynchronous callback function.
 
     @property
     def name(self):
@@ -77,8 +76,6 @@ class Event:
         return self._data
 
     def dumps(self):
-        """ 导出Json格式的数据
-        """
         d = {
             "n": self.name,
             "d": self.data
@@ -88,9 +85,6 @@ class Event:
         return b
 
     def loads(self, b):
-        """ 加载Json格式的bytes数据
-        @param b bytes类型的数据
-        """
         b = zlib.decompress(b)
         d = json.loads(b.decode("utf8"))
         self._name = d.get("n")
@@ -98,32 +92,25 @@ class Event:
         return d
 
     def parse(self):
-        """ 解析self._data数据
-        """
         raise NotImplemented
 
     def subscribe(self, callback, multi=False):
-        """ 订阅此事件
-        @param callback 回调函数
-        @param multi 是否批量订阅消息，即routing_key为批量匹配
+        """ Subscribe this event.
+
+        Args:
+            callback: Asynchronous callback function.
+            multi: If subscribe multiple channels ?
         """
         from quant.quant import quant
         self._callback = callback
         SingleTask.run(quant.event_center.subscribe, self, self.callback, multi)
 
     def publish(self):
-        """ 发布此事件
-        """
+        """Publish this event."""
         from quant.quant import quant
         SingleTask.run(quant.event_center.publish, self)
 
     async def callback(self, channel, body, envelope, properties):
-        """ 事件回调
-        @param channel 消息队列通道
-        @param body 接收到的消息
-        @param envelope 路由规则
-        @param properties 消息属性
-        """
         self._exchange = envelope.exchange_name
         self._routing_key = envelope.routing_key
         self.loads(body)
@@ -195,20 +182,22 @@ class EventHeartbeat(Event):
 
 
 class EventAsset(Event):
-    """ 资产更新事件
+    """ Asset event.
+
+    Attributes:
+        platform: Exchange platform name, e.g. bitmex.
+        account: Trading account name, e.g. test@gmail.com.
+        assets: Asset details.
+        timestamp: Publish time, microsecond.
+        update: If any update in this publish.
+
     * NOTE:
-        发布：资产服务
-        订阅：业务模块
+        Publisher: Asset server.
+        Subscriber: Any servers.
     """
 
     def __init__(self, platform=None, account=None, assets=None, timestamp=None, update=False):
-        """ 初始化
-        @param platform 交易平台
-        @param account 交易账户
-        @param assets 资产信息 {"BTC": {"free": "1.1", "locked": "2.2", "total": "3.3"}, ... }
-        @param timestamp 时间戳(毫秒)
-        @param update 资产是否更新 True 有更新 / False 无更新
-        """
+        """Initialize."""
         name = "EVENT_ASSET"
         exchange = "Asset"
         routing_key = "{platform}.{account}".format(platform=platform, account=account)
@@ -225,8 +214,6 @@ class EventAsset(Event):
         super(EventAsset, self).__init__(name, exchange, queue, routing_key, data=data)
 
     def parse(self):
-        """ 解析self._data数据
-        """
         asset = Asset(**self.data)
         return asset
 
@@ -292,25 +279,27 @@ class EventOrder(Event):
 
 
 class EventKline(Event):
-    """ K线更新事件 1分钟，5分钟，15分钟
+    """ Kline event.
+
+    Attributes:
+        platform: Exchange platform name, e.g. bitmex.
+        symbol: Trading pair, e.g. BTC/USD.
+        open: Open price.
+        high: Highest price.
+        low: Lowest price.
+        close: Close price.
+        volume: Trade volume.
+        timestamp: Publish time, microsecond.
+        kline_type: Kline type, kline/kline_5min/kline_15min.
+
     * NOTE:
-        发布：行情服务
-        订阅：业务服务
+        Publisher: Market server.
+        Subscriber: Any servers.
     """
 
     def __init__(self, platform=None, symbol=None, open=None, high=None, low=None, close=None, volume=None,
                  timestamp=None, kline_type=None):
-        """ 初始化
-        @param platform 平台
-        @param symbol 交易对
-        @param open 开盘价
-        @param high 最高价
-        @param low 最低价
-        @param close 收盘价
-        @param volume 成交量
-        @param timestamp 时间戳
-        @param kline_type K线类型 kline 1分钟K线，kline_5min 5分钟K线，kline_15min 15分钟K线
-        """
+        """Initialize."""
         if kline_type == const.MARKET_TYPE_KLINE:
             name = "EVENT_KLINE"
             exchange = "Kline"
@@ -341,22 +330,27 @@ class EventKline(Event):
         super(EventKline, self).__init__(name, exchange, queue, routing_key, data=data)
 
     def parse(self):
-        """ 解析self._data数据
-        """
         kline = Kline(**self.data)
         return kline
 
 
 class EventOrderbook(Event):
-    """ 订单薄事件
+    """ Orderbook event.
+
+    Attributes:
+        platform: Exchange platform name, e.g. bitmex.
+        symbol: Trading pair, e.g. BTC/USD.
+        asks: Asks, e.g. [[price, quantity], ... ]
+        bids: Bids, e.g. [[price, quantity], ... ]
+        timestamp: Publish time, microsecond.
+
     * NOTE:
-        订阅：业务模块
-        发布：行情服务
+        Publisher: Market server.
+        Subscriber: Any servers.
     """
 
     def __init__(self, platform=None, symbol=None, asks=None, bids=None, timestamp=None):
-        """ 初始化
-        """
+        """Initialize."""
         name = "EVENT_ORDERBOOK"
         exchange = "Orderbook"
         routing_key = "{platform}.{symbol}".format(platform=platform, symbol=symbol)
@@ -373,17 +367,24 @@ class EventOrderbook(Event):
         super(EventOrderbook, self).__init__(name, exchange, queue, routing_key, data=data)
 
     def parse(self):
-        """ 解析self._data数据
-        """
         orderbook = Orderbook(**self.data)
         return orderbook
 
 
 class EventTrade(Event):
-    """ 交易事件
+    """ Trade event.
+
+    Attributes:
+        platform: Exchange platform name, e.g. bitmex.
+        symbol: Trading pair, e.g. BTC/USD.
+        action: Trading side, BUY or SELL.
+        price: Order price.
+        quantity: Order size.
+        timestamp: Publish time, microsecond.
+
     * NOTE:
-        订阅：业务模块
-        发布：行情服务
+        Publisher: Market server.
+        Subscriber: Any servers.
     """
 
     def __init__(self, platform=None, symbol=None, action=None, price=None, quantity=None, timestamp=None):
@@ -406,14 +407,12 @@ class EventTrade(Event):
         super(EventTrade, self).__init__(name, exchange, queue, routing_key, data=data)
 
     def parse(self):
-        """ 解析self._data数据
-        """
         trade = Trade(**self.data)
         return trade
 
 
 class EventCenter:
-    """ 事件处理中心
+    """ Event center.
     """
 
     def __init__(self):
@@ -422,32 +421,35 @@ class EventCenter:
         self._username = config.rabbitmq.get("username", "guest")
         self._password = config.rabbitmq.get("password", "guest")
         self._protocol = None
-        self._channel = None  # 连接通道
-        self._connected = False  # 是否连接成功
-        self._subscribers = []  # 订阅者 [(event, callback, multi), ...]
-        self._event_handler = {}  # 事件对应的处理函数 {"exchange:routing_key": [callback_function, ...]}
+        self._channel = None  # Connection channel.
+        self._connected = False  # If connect success.
+        self._subscribers = []  # e.g. [(event, callback, multi), ...]
+        self._event_handler = {}  # e.g. {"exchange:routing_key": [callback_function, ...]}
 
-        LoopRunTask.register(self._check_connection, 10)  # 检查连接是否正常
+        # Register a loop run task to check TCP connection's healthy.
+        LoopRunTask.register(self._check_connection, 10)
 
     def initialize(self):
-        """ 初始化
-        """
         asyncio.get_event_loop().run_until_complete(self.connect())
 
     @async_method_locker("EventCenter.subscribe")
     async def subscribe(self, event: Event, callback=None, multi=False):
-        """ 注册事件
-        @param event 事件
-        @param callback 回调函数
-        @param multi 是否批量订阅消息，即routing_key为批量匹配
+        """ Subscribe a event.
+
+        Args:
+            event: Event type.
+            callback: Asynchronous callback.
+            multi: If subscribe multiple channel(routing_key) ?
         """
         logger.info("NAME:", event.name, "EXCHANGE:", event.exchange, "QUEUE:", event.queue, "ROUTING_KEY:",
                     event.routing_key, caller=self)
         self._subscribers.append((event, callback, multi))
 
     async def publish(self, event):
-        """ 发布消息
-        @param event 发布的事件对象
+        """ Publish a event.
+
+        Args:
+            event: A event to publish.
         """
         if not self._connected:
             logger.warn("RabbitMQ not ready right now!", caller=self)
@@ -456,14 +458,16 @@ class EventCenter:
         await self._channel.basic_publish(payload=data, exchange_name=event.exchange, routing_key=event.routing_key)
 
     async def connect(self, reconnect=False):
-        """ 建立TCP连接
-        @param reconnect 是否是断线重连
+        """ Connect to RabbitMQ server and create default exchange.
+
+        Args:
+            reconnect: If this invoke is a re-connection ?
         """
         logger.info("host:", self._host, "port:", self._port, caller=self)
         if self._connected:
             return
 
-        # 建立连接
+        # Create a connection.
         try:
             transport, protocol = await aioamqp.connect(host=self._host, port=self._port, login=self._username,
                                                         password=self._password)
@@ -471,7 +475,6 @@ class EventCenter:
             logger.error("connection error:", e, caller=self)
             return
         finally:
-            # 如果已经有连接已经建立好，那么直接返回（此情况在连续发送了多个连接请求后，若干个连接建立好了连接）
             if self._connected:
                 return
         channel = await protocol.channel()
@@ -480,33 +483,26 @@ class EventCenter:
         self._connected = True
         logger.info("Rabbitmq initialize success!", caller=self)
 
-        # 创建默认的交换机
+        # Create default exchanges.
         exchanges = ["Orderbook", "Trade", "Kline", "Kline.5min", "Kline.15min", "Config", "Heartbeat", "Asset",
                      "Order", ]
         for name in exchanges:
             await self._channel.exchange_declare(exchange_name=name, type_name="topic")
         logger.debug("create default exchanges success!", caller=self)
 
-        # 如果是断线重连，那么直接绑定队列并开始消费数据，如果是首次连接，那么等待5秒再绑定消费（等待程序各个模块初始化完成）
         if reconnect:
             self._bind_and_consume()
         else:
+            # Maybe we should waiting for all modules to be initialized successfully.
             asyncio.get_event_loop().call_later(5, self._bind_and_consume)
 
     def _bind_and_consume(self):
-        """ 绑定并开始消费事件消息
-        """
         async def do_them():
             for event, callback, multi in self._subscribers:
                 await self._initialize(event, callback, multi)
         SingleTask.run(do_them)
 
     async def _initialize(self, event: Event, callback=None, multi=False):
-        """ 创建/绑定交易所相关消息队列
-        @param event 订阅的事件
-        @param callback 回调函数
-        @param multi 是否批量订阅消息，即routing_key为批量匹配
-        """
         if event.queue:
             await self._channel.queue_declare(queue_name=event.queue, auto_delete=True)
             queue_name = event.queue
@@ -518,27 +514,18 @@ class EventCenter:
         await self._channel.basic_qos(prefetch_count=event.prefetch_count)
         if callback:
             if multi:
-                # 消费队列，routing_key为批量匹配，无需ack
                 await self._channel.basic_consume(callback=callback, queue_name=queue_name, no_ack=True)
                 logger.info("multi message queue:", queue_name, caller=self)
             else:
-                # 消费队列，routing_key唯一确定，需要ack确定
                 await self._channel.basic_consume(self._on_consume_event_msg, queue_name=queue_name)
                 logger.info("queue:", queue_name, caller=self)
                 self._add_event_handler(event, callback)
 
     async def _on_consume_event_msg(self, channel, body, envelope, properties):
-        """ 收到订阅的事件消息
-        @param channel 消息队列通道
-        @param body 接收到的消息
-        @param envelope 路由规则
-        @param properties 消息属性
-        """
         # logger.debug("exchange:", envelope.exchange_name, "routing_key:", envelope.routing_key,
         #              "body:", body, caller=self)
         try:
             key = "{exchange}:{routing_key}".format(exchange=envelope.exchange_name, routing_key=envelope.routing_key)
-            # 执行事件回调函数
             funcs = self._event_handler[key]
             for func in funcs:
                 SingleTask.run(func, channel, body, envelope, properties)
@@ -549,9 +536,6 @@ class EventCenter:
             await self._channel.basic_client_ack(delivery_tag=envelope.delivery_tag)  # response ack
 
     def _add_event_handler(self, event: Event, callback):
-        """ 增加事件处理回调函数
-        * NOTE: {"exchange:routing_key": [callback_function, ...]}
-        """
         key = "{exchange}:{routing_key}".format(exchange=event.exchange, routing_key=event.routing_key)
         if key in self._event_handler:
             self._event_handler[key].append(callback)
@@ -560,8 +544,6 @@ class EventCenter:
         logger.debug("event handlers:", self._event_handler.keys(), caller=self)
 
     async def _check_connection(self, *args, **kwargs):
-        """ 检查连接是否正常，如果连接已经断开，那么立即发起连接
-        """
         if self._connected and self._channel and self._channel.is_open:
             logger.debug("RabbitMQ connection ok.", caller=self)
             return
